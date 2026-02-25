1f19c1881b65a603867ca67010aa98460e89c033
shavuparmar <parmarshavu009@gmail.com>
2026-02-25 21:30:07 +0530

    authapi to api simple

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import { api } from "./client";
-export { api };
-export const authAPI = {
+
+export const api = {
   login: (payload) => api.post("/users/login", payload),
   me: () => api.get("/users/me"),
   logout: () => api.post("/users/logout"),
